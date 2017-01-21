5a73ee13d4379caa8904015ef2dae849298a103e
Viswanathan <saravanan.resume@gmail.com>
2017-01-22 02:57:42 +0800

    added first screen

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -1,53 +1,99 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: null,
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    movies: responseData.movies,
+                });
+            })
+            .done();
+    }
+
+
+    render() {
+        if (!this.state.movies) {
+            return this.renderLoadingView();
+        }
+
+        var movie = this.state.movies[0];
+        return this.renderMovie(movie);
+
+    }
+
+	renderLoadingView() {
+	    return (
+	      <View style={styles.container}>
+	        <Text>
+	          Loading movies...
+	        </Text>
+	      </View>
+	    );
+	}
+
+    renderMovie(movie) {
+        return (
+            <View style={styles.container}>
+              <Image
+                source={{uri: movie.posters.thumbnail}}
+                style={styles.thumbnail}
+              />
+              <View style={styles.rightContainer}>
+                <Text style={styles.title}>{movie.title}</Text>
+                <Text style={styles.year}>{movie.year}</Text>
+              </View>
+            </View>
+        );
+    }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    var styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#F5FCFF',
+        },
+        thumbnail: {
+            width: 53,
+            height: 81,
+        },
+        title: {
+            fontSize: 20,
+            marginBottom: 8,
+            textAlign: 'center',
+        },
+        year: {
+            textAlign: 'center',
+        },
+        rightContainer: {
+            flex: 1,
+        }
+    });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+    AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
