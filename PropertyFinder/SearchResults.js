a4041c3268dfd78bd2e5e9bddab70e0af3a637db
Viswanathan <saravanan.resume@gmail.com>
2017-01-24 02:37:14 +0800

    added images to SearchResults.js

--- PropertyFinder/SearchResults.js
+++ PropertyFinder/SearchResults.js
@@ -10,6 +10,8 @@ import {
   Text
 } from 'react-native';
 
+
+
 class SearchResults extends Component {
  
   constructor(props) {
@@ -21,26 +23,68 @@ class SearchResults extends Component {
     };
   }
  
-  renderRow(rowData, sectionID, rowID) {
-    return (
-      <TouchableHighlight
-          underlayColor='#dddddd'>
-        <View>
-          <Text>{rowData.title}</Text>
-        </View>
-      </TouchableHighlight>
-    );
-  }
+	renderRow(rowData, sectionID, rowID) {
+	  var price = rowData.price_formatted.split(' ')[0];
+
+	  return (
+		<TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+			underlayColor='#dddddd'>
+		  <View>
+			<View style={styles.rowContainer}>
+			  <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+			  <View  style={styles.textContainer}>
+				<Text style={styles.price}>{price}</Text>
+				<Text style={styles.title}
+					  numberOfLines={1}>{rowData.title}</Text>
+			  </View>
+			</View>
+			<View style={styles.separator}/>
+		  </View>
+		</TouchableHighlight>
+	  );
+	}
  
-  render() {
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}/>
-    );
-  }
+	render() {
+		return (
+		  <ListView
+			dataSource={this.state.dataSource}
+			renderRow={this.renderRow.bind(this)}/>
+		);
+	}
+  
+	rowPressed(listerURL) {
+	  var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+	}  
  
 }
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 module.exports = SearchResults;
 
