1a5b11a9f582c0767d4658075a65b904e71483fe
Viswanathan <saravanan.resume@gmail.com>
2017-03-04 23:21:30 +0800

    .

--- PropertyFinder/index.android.js
+++ PropertyFinder/index.android.js
@@ -1,53 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
+ 
+var React = require('react');
+var ReactNative = require('react-native');
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+var SearchPage = require('./SearchPage');
 
-export default class PropertyFinder extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+	class PropertyFinderApp extends React.Component {
+	  render() {
+		return (
+		  <ReactNative.NavigatorIOS
+			style={styles.container}
+			initialRoute={{
+			  title: 'Property Finder',
+			  component: SearchPage
+			}}/>
+		);
+	  }
+	}
 
-AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
+
+	var styles = ReactNative.StyleSheet.create({
+	  text: {
+		color: 'black',
+		backgroundColor: 'white',
+		fontSize: 30,
+		margin: 80
+	  },
+	  container: {
+		flex: 1
+	  }
+	});
+
+
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
+var SearchPage = require('./SearchPage');
